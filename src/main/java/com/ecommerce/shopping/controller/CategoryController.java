8d22013b92cde705b1142472f4fb240cf0538f0c
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-11-02 22:47:28 +0600

    addCategory method added in categoryController

--- src/main/java/com/ecommerce/shopping/controller/CategoryController.java
+++ src/main/java/com/ecommerce/shopping/controller/CategoryController.java
@@ -1,16 +1,16 @@
 package com.ecommerce.shopping.controller;
 
+import com.ecommerce.shopping.exception.AlreadyExistsException;
 import com.ecommerce.shopping.model.Category;
 import com.ecommerce.shopping.response.ApiResponse;
 import com.ecommerce.shopping.service.category.ICategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RequiredArgsConstructor
@@ -26,6 +26,16 @@ public class CategoryController {
             return ResponseEntity.ok(new ApiResponse("Found", categories));
         } catch (Exception e) {
             return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name) {
+        try {
+            Category theCategory = categoryService.addCategory(name);
+            return ResponseEntity.ok(new ApiResponse("Added", theCategory));
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null)) ;
         }
     }
 }
