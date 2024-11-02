e2dfb590001461d07ab3554f656359294e2603c1
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-11-02 19:31:15 +0600

    updateImage method added in ImageController

--- src/main/java/com/ecommerce/shopping/controller/ImageController.java
+++ src/main/java/com/ecommerce/shopping/controller/ImageController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.shopping.controller;
 
 import com.ecommerce.shopping.dto.ImageDTO;
+import com.ecommerce.shopping.exception.ResourceNotFoundException;
 import com.ecommerce.shopping.model.Image;
 import com.ecommerce.shopping.response.ApiResponse;
 import com.ecommerce.shopping.service.image.IImageService;
@@ -17,6 +18,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequiredArgsConstructor
@@ -41,6 +43,20 @@ public class ImageController {
         return ResponseEntity.ok().contentType((MediaType.parseMediaType(image.getFileType())))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" +image.getFileName()+"\"")
                 .body(resource);
+    }
+
+    @PutMapping("/image/{imageId}/update")
+    public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            if(image!=null) {
+                imageService.updateImage(file, imageId);
+                return ResponseEntity.ok(new ApiResponse("update success", null));
+            }
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("update failed", INTERNAL_SERVER_ERROR));
     }
 
 }
