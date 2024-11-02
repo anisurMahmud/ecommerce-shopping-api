9768b6fd8712964e2c9779b37855ab7f4e10e0dd
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-11-02 22:36:39 +0600

    getAll method in categoriesController

--- src/main/java/com/ecommerce/shopping/controller/CategoryController.java
+++ src/main/java/com/ecommerce/shopping/controller/CategoryController.java
@@ -1,7 +1,31 @@
 package com.ecommerce.shopping.controller;
 
+import com.ecommerce.shopping.model.Category;
+import com.ecommerce.shopping.response.ApiResponse;
+import com.ecommerce.shopping.service.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    private final ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error", INTERNAL_SERVER_ERROR));
+        }
+    }
 }
