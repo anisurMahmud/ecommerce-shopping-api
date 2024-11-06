faa4d9c329fdfe8398c5b1d4e20a9fb6e10c37c0
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-11-06 15:46:22 +0600

    getAllProducts method in ProductController added

--- src/main/java/com/ecommerce/shopping/controller/ProductController.java
+++ src/main/java/com/ecommerce/shopping/controller/ProductController.java
@@ -1,8 +1,27 @@
 package com.ecommerce.shopping.controller;
 
+import com.ecommerce.shopping.model.Product;
+import com.ecommerce.shopping.response.ApiResponse;
+import com.ecommerce.shopping.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+
+    private final IProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(new ApiResponse("Success", products));
+    }
 
 }
