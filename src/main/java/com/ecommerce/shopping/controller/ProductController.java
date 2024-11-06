c42e5c02952d3f437d7235b3cad049a62bf04732
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-11-06 15:58:38 +0600

    getProductById added in productController

--- src/main/java/com/ecommerce/shopping/controller/ProductController.java
+++ src/main/java/com/ecommerce/shopping/controller/ProductController.java
@@ -1,15 +1,19 @@
 package com.ecommerce.shopping.controller;
 
+import com.ecommerce.shopping.exception.ResourceNotFoundException;
 import com.ecommerce.shopping.model.Product;
 import com.ecommerce.shopping.response.ApiResponse;
 import com.ecommerce.shopping.service.product.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,6 +26,16 @@ public class ProductController {
     public ResponseEntity<ApiResponse> getAllProducts() {
         List<Product> products = productService.getAllProducts();
         return ResponseEntity.ok(new ApiResponse("Success", products));
+    }
+
+    @GetMapping("/product/{productId}/product")
+    public ResponseEntity<ApiResponse> getProductById(@PathVariable long productId) {
+        try {
+            Product product  = productService.getProductById(productId);
+            return ResponseEntity.ok(new ApiResponse("Success", product));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
     }
 
 }
