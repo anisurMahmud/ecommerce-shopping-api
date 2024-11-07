58d057be481111fe576aecb3aad54d0ec8bf3681
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-11-07 12:13:29 +0600

    model Mapper dependency added

--- src/main/java/com/ecommerce/shopping/dto/ProductDTO.java
+++ src/main/java/com/ecommerce/shopping/dto/ProductDTO.java
@@ -1,10 +1,12 @@
 package com.ecommerce.shopping.dto;
 
 import com.ecommerce.shopping.model.Category;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Data
 public class ProductDTO {
     private long id;
     private String name;
