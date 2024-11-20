d57175c226b25a9dbb8cff19d2ac906806edd246
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-11-20 22:35:53 +0600

    cart and cartitem model done

--- src/main/java/com/ecommerce/shopping/model/Cart.java
+++ src/main/java/com/ecommerce/shopping/model/Cart.java
@@ -1,4 +1,17 @@
 package com.ecommerce.shopping.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> cartItems;
 }
--- src/main/java/com/ecommerce/shopping/model/CartItem.java
+++ src/main/java/com/ecommerce/shopping/model/CartItem.java
@@ -1,4 +1,24 @@
 package com.ecommerce.shopping.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
 public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
 }
