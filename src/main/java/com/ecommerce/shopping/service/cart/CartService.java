b753fd7650e6c38c1ed7cbbe28695ab360c74cff
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-11-21 14:52:08 +0600

    base method addded in Cartservice class and interface

--- src/main/java/com/ecommerce/shopping/service/cart/CartService.java
+++ src/main/java/com/ecommerce/shopping/service/cart/CartService.java
@@ -1,4 +1,25 @@
 package com.ecommerce.shopping.service.cart;
 
-public class CartService {
+import com.ecommerce.shopping.model.Cart;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class CartService implements ICartService {
+
+    @Override
+    public Cart getCart(Long id) {
+        return null;
+    }
+
+    @Override
+    public void clearCart(Long id) {
+
+    }
+
+    @Override
+    public BigDecimal getTotalPrice(Long id) {
+        return null;
+    }
 }
--- src/main/java/com/ecommerce/shopping/service/cart/ICartService.java
+++ src/main/java/com/ecommerce/shopping/service/cart/ICartService.java
@@ -1,4 +1,11 @@
 package com.ecommerce.shopping.service.cart;
 
+import com.ecommerce.shopping.model.Cart;
+
+import java.math.BigDecimal;
+
 public interface ICartService {
+    Cart getCart(Long id);
+    void clearCart(Long id);
+    BigDecimal getTotalPrice(Long id);
 }
