4630ce5deb5f3bfa6b08c6265fce9894b3b310c8
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-11-21 15:09:25 +0600

    repository added in cartService

--- src/main/java/com/ecommerce/shopping/service/cart/CartService.java
+++ src/main/java/com/ecommerce/shopping/service/cart/CartService.java
@@ -1,12 +1,17 @@
 package com.ecommerce.shopping.service.cart;
 
 import com.ecommerce.shopping.model.Cart;
+import com.ecommerce.shopping.repository.CartRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
+@RequiredArgsConstructor
 @Service
 public class CartService implements ICartService {
+
+    private final CartRepository cartRepository;
 
     @Override
     public Cart getCart(Long id) {
