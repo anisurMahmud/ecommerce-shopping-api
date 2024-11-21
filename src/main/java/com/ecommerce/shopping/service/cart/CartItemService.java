8cba7990a4421fac0f4066e841e6026cbb46187a
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-11-21 15:46:39 +0600

    base method added for cartItemService and interface

--- src/main/java/com/ecommerce/shopping/service/cart/CartItemService.java
+++ src/main/java/com/ecommerce/shopping/service/cart/CartItemService.java
@@ -1,4 +1,27 @@
 package com.ecommerce.shopping.service.cart;
 
-public class CartItemService {
+import com.ecommerce.shopping.repository.CartItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CartItemService implements ICartItemService {
+
+    private final CartItemRepository cartItemRepository;
+
+    @Override
+    public void addItemtoCart(Long cartId, Long productId, int quantity) {
+
+    }
+
+    @Override
+    public void removeItemfromCart(Long cartId, Long productId) {
+
+    }
+
+    @Override
+    public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+
+    }
 }
--- src/main/java/com/ecommerce/shopping/service/cart/ICartItemService.java
+++ src/main/java/com/ecommerce/shopping/service/cart/ICartItemService.java
@@ -1,4 +1,7 @@
 package com.ecommerce.shopping.service.cart;
 
 public interface ICartItemService {
+    void addItemtoCart(Long cartId, Long productId, int quantity);
+    void removeItemfromCart(Long cartId, Long productId);
+    void updateItemQuantity(Long cartId, Long productId, int quantity);
 }
