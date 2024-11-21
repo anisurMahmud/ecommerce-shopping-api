9890fbcae3df8471f29f9fbc1483f45f4a787c07
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-11-21 15:16:21 +0600

    modified cart model

--- src/main/java/com/ecommerce/shopping/model/Cart.java
+++ src/main/java/com/ecommerce/shopping/model/Cart.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -21,5 +22,32 @@ public class Cart {
     private BigDecimal totalAmount = BigDecimal.ZERO;
 
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<CartItem> cartItems;
+    private Set<CartItem> cartItems = new HashSet<>();
+    
+    public void addItem(CartItem item) {
+        this.cartItems.add(item);
+        item.setCart(this);
+        updateTotalAmount();
+    }
+
+    public void removeItem(CartItem item) {
+        this.cartItems.remove(item);
+        item.setCart(null);
+        updateTotalAmount();
+    }
+
+    private void updateTotalAmount() {
+        this.totalAmount = cartItems.stream().map(cartItem -> {
+            BigDecimal unitPrice = cartItem.getUnitPrice();
+            if(unitPrice == null) {
+                return BigDecimal.ZERO;
+            }
+            return unitPrice.multiply(new BigDecimal(cartItem.getQuantity()));
+        }).reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public void clearCart(){
+        this.cartItems.clear();
+        updateTotalAmount();
+    }
 }
