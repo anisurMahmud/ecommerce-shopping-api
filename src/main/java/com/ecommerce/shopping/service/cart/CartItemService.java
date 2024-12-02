03022aa00eb4a7dfceb400d638a86bd3ce7bea2a
anisurMahmud <19202103107@cse.bubt.edu.bd>
2024-12-02 16:48:18 +0600

    cartItemService method implementation done

--- src/main/java/com/ecommerce/shopping/service/cart/CartItemService.java
+++ src/main/java/com/ecommerce/shopping/service/cart/CartItemService.java
@@ -1,5 +1,6 @@
 package com.ecommerce.shopping.service.cart;
 
+import com.ecommerce.shopping.exception.ResourceNotFoundException;
 import com.ecommerce.shopping.model.Cart;
 import com.ecommerce.shopping.model.CartItem;
 import com.ecommerce.shopping.model.Product;
@@ -9,7 +10,7 @@ import com.ecommerce.shopping.service.product.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -47,11 +48,35 @@ public class CartItemService implements ICartItemService {
 
     @Override
     public void removeItemfromCart(Long cartId, Long productId) {
-
+        Cart cart = cartService.getCart(cartId);
+        CartItem itemToRemove = getCartItem(cartId, productId);
+        cart.removeItem(itemToRemove);
+        cartRepository.save(cart);
     }
 
     @Override
     public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        cart.getCartItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .ifPresent(item -> {
+                    item.setQuantity(quantity);
+                    item.setUnitPrice(item.getProduct().getPrice());
+                    item.setTotalPrice();
+                });
+        BigDecimal totalAmount = cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+        cartRepository.save(cart);
+    }
 
+    @Override
+    public CartItem getCartItem(Long cartId, Long productId) {
+        Cart cart = cartService.getCart(cartId);
+        return cart.getCartItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst().orElseThrow(() -> new ResourceNotFoundException("item not found"));
     }
 }
--- src/main/java/com/ecommerce/shopping/service/cart/ICartItemService.java
+++ src/main/java/com/ecommerce/shopping/service/cart/ICartItemService.java
@@ -1,7 +1,11 @@
 package com.ecommerce.shopping.service.cart;
 
+import com.ecommerce.shopping.model.CartItem;
+
 public interface ICartItemService {
     void addItemtoCart(Long cartId, Long productId, int quantity);
     void removeItemfromCart(Long cartId, Long productId);
     void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+    CartItem getCartItem(Long cartId, Long productId);
 }
